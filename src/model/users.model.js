e1e46aa9d099f327cceb4b0cd80c0304b8c44f74
Nguyễn Trung <ITITIU20332@student.hcmiu.edu.vn>
2024-12-06 23:41:15 +0700

    06/12/2024: Mess room

--- src/model/users.model.js
+++ src/model/users.model.js
@@ -19,9 +19,139 @@ module.exports = (sequelize, Sequelize) => {
                 type: Sequelize.STRING,
                 allowNull: false,
             },
+            slogan: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
             avt_file_path: {
                 type: Sequelize.STRING,
                 allowNull: true,
+            },
+            dob: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+            school: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            major: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            needs: {
+                type: Sequelize.ENUM('findTutor', 'studyBuddy', 'sharedHobby'),
+                allowNull: true,
+            },
+            sex: {
+                type: Sequelize.ENUM('male', 'female', 'other', 'prefer_not_to_say'),
+                allowNull: false,
+                defaultValue: 'male',
+            },
+            music: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            game: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            sing: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            eat: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            exercise: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            running: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            badminton: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            walking: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            beach: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            hiking: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            travel: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            reading: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            pets: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            basketball: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            pickelBall: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            horror: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            anime: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            romance: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            action: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            detective: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            fantasy: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
             },
         },
         {
